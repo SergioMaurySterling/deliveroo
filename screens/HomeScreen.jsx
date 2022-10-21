c9eacb016d8c82ee42dfdcf4468a45696a66cf6c
Sergio Maury <psergio060897@gmail.com>
2022-10-21 17:57:22 -0500

    icons

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,6 +1,12 @@
 import React, {useLayoutEffect} from 'react';
 import { View, Text, SafeAreaView, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon
+} from 'react-native-heroicons/outline'
 
 function HomeScreen() {
   const navigation = useNavigation();
@@ -15,11 +21,18 @@ function HomeScreen() {
     //The safe area view set all the content below the top bar on phones.
     <SafeAreaView>
       {/* Header */}
-      <View>
+      <View className='flex-row pb-3 items-center mx-4 space-x-2'>
         <Image
           source={{uri:'https://links.papareact.com/wru'}}
           className='h-7 w-7 bg-gray-300 p-4 rounded-full'
         />
+        <View>
+          <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+          <Text className='font-bold text-xl'>
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
       </View>
     </SafeAreaView>
   );
